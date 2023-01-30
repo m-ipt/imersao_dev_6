01cb5c26dc5dba6b666f8afe64be9e3fec8547fc
Maria Isabel <mipt1612@gmail.com>
2023-01-30 19:55:08 -0300

    Comentar parte do tempo da viagem estelar

--- anos-luz.js
+++ anos-luz.js
@@ -6,11 +6,13 @@ var km = 9500000000000;
 var anosLuz = km * anos;
 alert(anos + " equivalem a " + anosLuz + " quilômetros");
 
-alert("Agora vamos checar quanto tempo levaria para chegarmos de uma estrela a outra. Vamos lá!");
+// ATENÇÃO: A PARTE ABAIXO ESTÁ EM CONSTRUÇÃO, POR ISSO ESTÁ TODA COMENTADA. //
+
+// alert("Agora vamos checar quanto tempo levaria para chegarmos de uma estrela a outra. Vamos lá!") //
 
 // A distância que um fóton (uma partícula de luz) percorre durante um ano no vácuo. Um ano-luz é o comprimento da distância percorrida pela luz em um ano no vácuo. 9.500.000.000.000 quilômetros //
 
-var estrela = prompt("Insira aqui a distância de uma estrela para a outra (em anos-luz)");
-var tempo = estrela;
-total = tempo * 31667000
-alert("O tempo que ")
+// var estrela = prompt("Insira aqui a distância de uma estrela para a outra (em anos-luz)") //
+// var tempo = estrela //
+// total = tempo * //
+//alert("O tempo que ") //
