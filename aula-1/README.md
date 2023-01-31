141b3fc0372c7674bfdfab7fbe8aaf1214271939
Maria Isabel <76397432+m-ipt@users.noreply.github.com>
2023-01-31 13:44:27 -0300

    Adicionar arquivos da aula 1

---
+++ aula-1/README.md
@@ -0,0 +1,2 @@
+# imersao-dev-6
+Códigos criados pela Imersão Dev de edição 6 da Alura
---
+++ aula-1/anos-luz.js
@@ -0,0 +1,16 @@
+//BEM-VINDO OU BEM-VINDA AO CÓDIGO//
+
+var converter = prompt("Insira um número de anos-luz para convertermos em quilômetros");
+var anos = converter;
+var km = 9500000000000;
+var anosLuz = km * anos;
+alert(anos + " equivalem a " + anosLuz + " quilômetros");
+
+alert("Agora vamos checar quanto tempo levaria para chegarmos de uma estrela a outra. Vamos lá!")
+
+// A distância que um fóton (uma partícula de luz) percorre durante um ano no vácuo. Um ano-luz é o comprimento da distância percorrida pela luz em um ano no vácuo. 9.500.000.000.000 quilômetros //
+
+var estrela = prompt("Insira aqui a distância de uma estrela para a outra (em anos-luz)")
+var tempo = estrela
+total = tempo * 0.00000000000010570
+alert("O tempo que levaria de uma estrela para outra é " + total + " na velocidade da luz")
